2454d7f6a7a4b98fdae5776540b0fbbeb0fae98b
luuthikieuoanh <67403607+luuthikieuoanh@users.noreply.github.com>
2020-10-21 11:02:50 +0700

    hihi

---
+++ main/java/tdc/edu/vn/caculator/ChooseOperatorActivity.java
@@ -0,0 +1,13 @@
+package tdc.edu.vn.caculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseOperatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_operator_layout);
+    }
+}
